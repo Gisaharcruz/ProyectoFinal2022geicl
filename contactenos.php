449e4d8c19ceb4fbafb69cdf7a2b170d59922d29
Gisaharcruz <cruzgilmar604@gmail.com>
2022-08-16 07:40:57 -0600

    decima parte subida

--- contactenos.php
+++ contactenos.php
@@ -7,46 +7,60 @@
     <title>Contact Us</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="stylecontactenos.css">
-    <link rel="stylesheet" href="#"> <!-- Poner Estilo de Insertar los Datos -->
+    <link rel="stylesheet" href="contacto.css">
     <link rel="stylesheet" href="footer.css">
 </head>
 <body>
 <a href="home.php" class="btn10">Inicio</a>
 <a href="productos.php" class="btn11">Productos</a>
 <a href="iniciosesionmensajes.php" class="btn12">Iniciar Sesion</a>
-<a href="mensajes.php" class="btn13">Mensajes</a>
+<!--<a href="mensajes.php" class="btn13">Mensajes</a>-->
 <br><br>
 
-<section class="form-main">
-            <div class="form-content">
-            <div class="circle-1"></div>
-            <div class="circle-2"></div>
-            <div class="circle-3"></div>
-            <div class="box">
-                <h3>Contactenos</h3>
-                <br>
-                <form action="insertarcontacto.php" method="POST">
-                    <p class="text-start">Nombre</p>
-                    <div class="input-box">
-                        <input type="text" name="nombre" placeholder="Introduzca su Nombre & Apellido" class="input-control">
-                    </div>
-                    <p class="text-start">Telefono/Celular</p>
-                    <div class="input-box">
-                        <input type="text" name="telefono" placeholder="Introduzca su Numero de Telefono" class="input-control">
-                    </div>
-                    <p class="text-start">Correo Electronico</p>
-                    <div class="input-box">
-                        <input type="text" name="correo" placeholder="Introduzca su Correo Electronico" class="input-control">
-                    </div>
-                    <p class="text-start">Mensaje</p>
-                    <div class="input-box">
-                        <input type="text" name="mensaje" placeholder="Introduzca su Mensaje" class="input-control">
-                    </div>
-                    <button type="submit" name="aceptar" class="btn">Enviar Mensaje</button>
-                </form>
-            </div>
-        </div>
-    </section>
+<h3 class="h3">Contactenos</h3>
+  <!-- formulario de contacto en html y css -->  
+  <div class="contact_form">
+
+    <div class="formulario">      
+
+
+          <form action="insertarcontacto.php" method="post">       
+
+            
+                <p>
+                  <label for="nombre" class="colocar_nombre">Nombre
+                    <span class="obligatorio">*</span>
+                  </label>
+                    <input type="text" name="introducir_nombre" id="nombre" required="obligatorio" placeholder="Escribe tu Nombre">
+                </p>
+            
+                <p>
+                  <label for="telefone" class="colocar_telefono">Telefono/Celular
+                  <span class="obligatorio">*</span>
+                  </label>
+                    <input type="tel" name="introducir_telefono" id="celular" placeholder="Escribe tu Correo">
+                </p>  
+
+                <p>
+                  <label for="email" class="colocar_email">Correo Electronico
+                    <span class="obligatorio">*</span>
+                  </label>
+                    <input type="email" name="introducir_email" id="correo" required="obligatorio" placeholder="Escribe tu Telefono">
+                </p>
+
+                <p>
+                  <label for="mensaje" class="colocar_mensaje">Mensaje
+                    <span class="obligatorio">*</span>
+                  </label>                     
+                                    <textarea name="introducir_mensaje" class="texto_mensaje" id="mensaje" required="obligatorio" placeholder="Deja aquí tu comentario..."></textarea> 
+                                </p>                    
+              
+                <button type="submit" name="enviar_formulario" id="enviar"><p>Enviar</p></button>
+        
+            
+          </form>
+    </div>  
+  </div>
     <br>
 
     <footer class="footer">
--- iniciosesionmensajes.php
+++ iniciosesionmensajes.php
@@ -14,13 +14,15 @@
             <div class="circle-2"></div>
             <div class="circle-3"></div>
             <div class="box">
-                <h3>¡Bienvenido Admin!</h3>
+                <h3>Iniciar Sesión</h3>
                 <form action="mensajes.php" method="POST">
                     <div class="input-box">
+                        <h4>Usuario</h4><br>
                         <input type="text" placeholder="Usuario" class="input-control">
                     </div>
                     <br>
                     <div class="input-box">
+                        <h4>Contraseña</h4><br>
                         <input type="password" placeholder="Contraseña" class="input-control">
                     </div>
                     <br>
