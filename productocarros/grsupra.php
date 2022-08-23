809c6d47a6f115e24416b11011d3a9e7db647508
Gisaharcruz <cruzgilmar604@gmail.com>
2022-08-22 21:28:44 -0600

    terceava parte subida

--- productocarros/grsupra.php
+++ productocarros/grsupra.php
@@ -8,9 +8,83 @@
     <link rel="stylesheet" href="grsupra.css">
 </head>
 <body>
-    <div class="center">
-    <img class="img" src="images/supra.jpg" alt="">
-    </div>
-    <h3 class="subtitulo">Toyota GR Supra</h3>
+    <header class="hero">
+        <nav class="nav__hero">
+            <div class="container nav__container">
+                <div class="logo">
+                    <h2 class="logo__name">Toyota GR Supra<span class="point">.</span></h2>
+                </div>
+            </div>
+        </nav>
+
+        <section class="container hero_main">
+            <div class="hero__textos">
+                <h1 class="title">Datos Tecnicos del <span class="title__active">Toyota GR Supra</span></h1>
+                <p class="copy">Nos encargamos que usted tenga la mejor experiencia <span class="copy__active">de su vida.</span></p>
+                <a href="#" class="cta">Compra tu carro</a>
+            </div>
+            <img src="../images/supra.jpg" alt="">
+        </section>
+    </header>
+
+    <main>
+        <section class="services">
+            <div class="container">
+                <h2 class="subtitle">Nuestros servicios<span class="point">.</span> </h2>
+                <p class="copy__section">Aprovecha nustros descuentos de medio año, llevate tu carro de tus sueños.</p>
+
+                <article class="container-card">
+                    <div class="card">
+                        <img src="../images/supra.jpg" class="card__img">
+                        <div class="cards__text">
+                            <p class="card__list">Especialidades en carros toyota</p>
+                            <h3 class="card__title">Web Toyota<span class="point">.</span></h3>
+                            <p class="card__copy">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus deserunt sit soluta enim quidem accusantium, commodi ad similique optio ipsam beatae maiores voluptate odit officia vel molestias, officiis ducimus nam.</p>
+                            <a href="#" class="card__button">Comienza tu sueño con Toyota</a>
+                        </div>
+
+                        <div class="card">
+                        <img src="../images/supra.jpg" class="card__img">
+                        <div class="cards__text">
+                            <p class="card__list">Especialidades en carros toyota</p>
+                            <h3 class="card__title">Web Toyota<span class="point">.</span></h3>
+                            <p class="card__copy">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus deserunt sit soluta enim quidem accusantium, commodi ad similique optio ipsam beatae maiores voluptate odit officia vel molestias, officiis ducimus nam.</p>
+                            <a href="#" class="card__button">Comienza tu sueño con Toyota</a>
+                        </div>
+
+                        <div class="card">
+                        <img src="../images/supra.jpg" class="card__img">
+                        <div class="cards__text">
+                            <p class="card__list">Especialidades en carros toyota</p>
+                            <h3 class="card__title">Web Toyota<span class="point">.</span></h3>
+                            <p class="card__copy">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus deserunt sit soluta enim quidem accusantium, commodi ad similique optio ipsam beatae maiores voluptate odit officia vel molestias, officiis ducimus nam.</p>
+                            <a href="#" class="card__button">Comienza tu sueño con Toyota</a>
+                        </div>
+                    </div>
+                </article>
+            </div>
+        </section>
+
+        <section class="proyects container">
+            <h2 class="subtitle">Nuestros servicios<span class="point">.</span> </h2>
+            <p class="copy__section">Aprovecha nustros descuentos de medio año, llevate tu carro de tus sueños.</p>
+            <article class="container-bg">
+            <div class="card">
+                <div class="cards__text">
+                <h3 class="card__title">Diseño Web Toyota<span class="point">.</span></h3>
+                <p class="card__date">22 de agosto de 2022</p>
+                <p class="card__copy">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus deserunt sit soluta enim quidem accusantium, commodi ad similique optio ipsam beatae maiores voluptate odit officia vel molestias, officiis ducimus nam.</p>
+                <a href="#" class="card__button">Comienza tu sueño con Toyota</a>
+                </div>
+            </div>
+            <div class="background">
+                <img src="../images/supra.jpg" class="background__img">
+                <div class="background__text">
+                    <h3 class="background">El mejor GR Supra del 2022</h3>
+                </div>
+            </div>
+            </article>
+        </section>
+    </main>
 </body>
 </html>
